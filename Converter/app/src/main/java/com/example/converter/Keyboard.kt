abe664c8aec3edb2182c728a9c642a8d92b830d0
Maria Pugacheva <mpouga@gmail.com>
2020-09-30 22:17:04 +0300

    Binding added

--- Converter/app/src/main/java/com/example/converter/Keyboard.kt
+++ Converter/app/src/main/java/com/example/converter/Keyboard.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.converter.databinding.FragmentKeyboardBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +22,11 @@ class Keyboard : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var activityCallback: MainActivity
+    private var _binding: FragmentKeyboardBinding? = null
+    private val binding get() = _binding!!
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -33,8 +39,9 @@ class Keyboard : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_keyboard, container, false)
+        _binding = FragmentKeyboardBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
     companion object {
