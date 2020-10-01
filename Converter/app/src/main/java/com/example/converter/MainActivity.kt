91eab2ca63eca470dc8591b7e453e52d4e6bb53f
Maria Pugacheva <mpouga@gmail.com>
2020-10-01 23:58:10 +0300

    Input, delete added

--- Converter/app/src/main/java/com/example/converter/MainActivity.kt
+++ Converter/app/src/main/java/com/example/converter/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.converter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.converter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -11,7 +15,23 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun keyboardClicked(num: Int) {
-        val a = num
+        val fragmentMain = supportFragmentManager.findFragmentById(R.id.fragment_main) as MainFragment
+        var str: String = fragmentMain.getInputText()
+
+        when (num) {
+            in 0..9 -> str += num.toString()
+            10 -> str = DeleteChar(str)
+            11 -> print("")
+        }
+        fragmentMain.setInputText(str)
+    }
+
+    private fun DeleteChar(str: String): String {
+        val len = str.length
+        when (len) {
+            0, 1 -> return ""
+            else -> return str.substring(0..(len - 2))
+        }
     }
 
     companion object {
